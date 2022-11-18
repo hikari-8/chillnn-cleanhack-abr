5d49eb5936ae7ebb5fbb9f82b208330b33e2366c
hikari-8 <kanbarusuruga0127@gmail.com>
2022-11-18 22:49:16 +0900

    enhance/2022-11-17/レポジトリをエクスポートしました

--- src/entities/repositories/index.ts
+++ src/entities/repositories/index.ts
@@ -1,13 +1,16 @@
 import { IUserMastRepository } from "./modules/userMastRepository";
 import { ICleanPlaceMastRepository } from "./modules/cleanPlaceMastRepository";
+import { IWeeklyRecordMastRepository } from "./modules/weeklyRecordMastRepository";
 
 export * from "./modules/userMastRepository";
 export * from "./modules/cleanPlaceMastRepository";
+export * from "./modules/weeklyRecordMastRepository";
 
 export class RepositoryContainer {
 	constructor(
 		// entity
 		public userMastRepository: IUserMastRepository,
-		public cleanPlaceMastRepository: ICleanPlaceMastRepository
+		public cleanPlaceMastRepository: ICleanPlaceMastRepository,
+		public weeklyRecordMastRepository: IWeeklyRecordMastRepository
 	) {}
 }
