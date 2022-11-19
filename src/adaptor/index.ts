54c4d9486a0c07765b579ea23d5bf31fdf8173f7
hikari-8 <kanbarusuruga0127@gmail.com>
2022-11-19 17:18:39 +0900

    enhance/2022-11-19/cacheadaptorをエクスポートしました

--- src/adaptor/index.ts
+++ src/adaptor/index.ts
@@ -1,1 +1,4 @@
-export * from "./repositoryCacheAdaptor/userMastRepository";
+export * from "./repositoryCacheAdaptor/userMastRepositoryCacheAdaptor";
+export * from "./repositoryCacheAdaptor/cleanPlaceMastRepositoryCacheAdaptor";
+export * from "./repositoryCacheAdaptor/weeklyRecordMastRepositoryCacheAdaptor";
+export * from "./repositoryCacheAdaptor/roomMastRepositoryCacheAdaptor";
