0818c2f59737b4747db198ed920ab87272df760b
hikari-8 <kanbarusuruga0127@gmail.com>
2022-11-24 18:41:34 +0900

    enhance/2022-11-24/groupMastのChacheAdaptorを変更しました

---
+++ src/adaptor/repositoryCacheAdaptor/groupMastRepositoryCacheAdaptor.ts
@@ -0,0 +1,63 @@
+import { IGroupMastRepository, GroupMast, Scalars } from "../../entities";
+
+type Groupcache = {
+	[groupID: string]: GroupMast | "blanc" | undefined;
+};
+
+export class GroupMastRepositoryCacheAdaptor implements IGroupMastRepository {
+	private GroupEachCache: Groupcache = {};
+	private GroupAllCache: Groupcache | null = null;
+	private mygroupID: string | null = null;
+	constructor(private repository: IGroupMastRepository) {}
+
+	async addGroup(input: GroupMast): Promise<GroupMast> {
+		const res = await this.repository.addGroup(input);
+		res.createdAt = new Date().getDate();
+		this.addCacheEach(input.groupID, res);
+		this.mygroupID = input.groupID;
+		return res;
+	}
+
+	async deleteGroup(groupID: string): Promise<GroupMast> {
+		const res = await this.repository.deleteGroup(groupID);
+		res.deletedAt = new Date().getDate();
+		this.addCacheEach(groupID, res);
+		return res;
+	}
+
+	async updateGroup(input: GroupMast): Promise<GroupMast> {
+		const res = await this.repository.updateGroup(input);
+		res.updatedAt = new Date().getDate();
+		this.addCacheEach(input.groupID, res);
+		this.mygroupID = input.groupID;
+		return res;
+	}
+
+	async fetchGroupByGroupID(groupID: string): Promise<GroupMast | null> {
+		const cache = this.fetchGroup(groupID);
+		if (cache && cache === "blanc") {
+			return null;
+		} else if (cache) {
+			return cache;
+		}
+		const res = await this.repository.fetchGroupByGroupID(groupID);
+		this.addCacheEach(groupID, res);
+		return res;
+	}
+
+	// ===============================================================
+	//
+	// private
+	//
+	// ===============================================================
+	private addCacheEach(groupID: Scalars["ID"], Group: GroupMast | null) {
+		this.GroupEachCache[groupID] = Group || "blanc";
+		if (this.GroupAllCache && Group) {
+			this.GroupAllCache[groupID] = Group;
+		}
+	}
+
+	private fetchGroup(groupID: Scalars["ID"]) {
+		return this.GroupEachCache[groupID];
+	}
+}
